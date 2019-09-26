6748b7b434c2c086f7be0437babc88bb4087cd06
DzianisH <gavrilovetsden@gmail.com>
2019-09-26 21:57:41 +0300

    Add logging to file

--- logger.js
+++ logger.js
@@ -1,15 +1,31 @@
+const fs = require('fs');
+
 module.exports = (ctx, next) => {
     const start = new Date()
     return next(ctx)
         .then(
             () => {
                 const ms = new Date() - start
-                console.log(createLogMsg(ctx, ms));
+                const msg = createLogMsg(ctx, ms);
+                fs.appendFile('info.log', msg + '\n', err => {
+                    if(err) {
+                        console.error("Can't log message to info.log");
+                        console.error(err);
+                    }
+                });
+                console.log(msg);
             },
             () => {
                 const ms = new Date() - start
-                console.error(createLogMsg(ctx, ms));            }
-            );
+                const msg = createLogMsg(ctx, ms);
+                fs.appendFile('error.log', msg + '\n', err => {
+                    if(err) {
+                        console.error("Can't log message to error.log");
+                        console.error(err);
+                    }
+                });
+                console.error(msg);
+            });
 }
 
 const createLogMsg = (ctx, ms) => {
